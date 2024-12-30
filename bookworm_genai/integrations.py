ebb2bd5467d923d5c814ac7655f129242948e9d3
Kiran Patel <7103956+kiran94@users.noreply.github.com>
2024-12-30 19:17:50 +0000

    refactor(firefox): ensure source is tracked (#24)
    * feat(firefox): ensure source file is tracked
    
    * refactor(metadata): add version
    
    * test(utils): verify
    
    * test(utils): cover darwin

--- bookworm_genai/integrations.py
+++ bookworm_genai/integrations.py
@@ -6,6 +6,7 @@ from langchain_community.document_loaders import JSONLoader
 from langchain_community.document_loaders.sql_database import SQLDatabaseLoader
 from langchain_community.utilities.sql_database import SQLDatabase
 
+from bookworm_genai.utils import CHROMIUM_JQ_COMMAND, sql_loader_page_content_mapper, sql_loader_firefox_copy_path, sql_loader_firefox_sql_query
 
 class Browser(str, Enum):
     BRAVE = "brave"
@@ -15,15 +16,6 @@ class Browser(str, Enum):
     @classmethod
     def list(cls):
         return list(map(lambda c: c.value, cls))
-
-
-_CHROMIUM_JQ_COMMAND = """
-  [.roots.bookmark_bar.children, .roots.other.children] |
-  flatten |
-  .. |
-  objects |
-  select(.type == "url")
-"""
 
 BrowserManifest = dict[Browser, dict[str, dict[str, Any]]]
 
@@ -39,100 +31,66 @@ browsers: BrowserManifest = {
             "bookmark_loader": JSONLoader,
             "bookmark_loader_kwargs": {
                 "file_path": os.path.expanduser("~/.config/BraveSoftware/Brave-Browser/Default/Bookmarks"),
-                "jq_schema": _CHROMIUM_JQ_COMMAND,
+                "jq_schema": CHROMIUM_JQ_COMMAND,
                 "text_content": False,
             },
         },
-        # "win32": {},
         "darwin": {
             "bookmark_loader": JSONLoader,
             "bookmark_loader_kwargs": {
                 "file_path": os.path.expanduser("~/Library/Application Support/BraveSoftware/Brave-Browser/Default/Bookmarks"),
-                "jq_schema": _CHROMIUM_JQ_COMMAND,
+                "jq_schema": CHROMIUM_JQ_COMMAND,
                 "text_content": False,
             },
         },
+        # "win32": {},
     },
     Browser.CHROME: {
         "linux": {
             "bookmark_loader": JSONLoader,
             "bookmark_loader_kwargs": {
                 "file_path": os.path.expanduser("~/.config/google-chrome/Default/Bookmarks"),
-                "jq_schema": _CHROMIUM_JQ_COMMAND,
+                "jq_schema": CHROMIUM_JQ_COMMAND,
                 "text_content": False,
             },
         },
-        # "win32": {},
         "darwin": {
             "bookmark_loader": JSONLoader,
             "bookmark_loader_kwargs": {
                 "file_path": os.path.expanduser("~/Library/Application Support/Google/Chrome/Default/Bookmarks"),
-                "jq_schema": _CHROMIUM_JQ_COMMAND,
+                "jq_schema": CHROMIUM_JQ_COMMAND,
                 "text_content": False,
             },
         },
+        # "win32": {},
     },
     Browser.FIREFOX: {
         "linux": {
             "bookmark_loader": SQLDatabaseLoader,
             "bookmark_loader_kwargs": {
                 "db": lambda _: SQLDatabase.from_uri("sqlite:////tmp/bookworm/firefox.sqlite"),
-                "query": """
-                    SELECT
-                       CAST(moz_places.id AS TEXT) AS id,
-                       moz_bookmarks.title,
-                       moz_places.url,
-                       CAST(moz_bookmarks.dateAdded AS TEXT) AS dateAdded,
-                       CAST(moz_bookmarks.lastModified AS TEXT) AS lastModified
-                    FROM
-                       moz_bookmarks
-                    LEFT JOIN
-                       moz_places
-                    ON
-                       moz_bookmarks.fk = moz_places.id
-                    WHERE
-                       moz_bookmarks.type = 1
-                    AND
-                       moz_bookmarks.title IS NOT NULL
-                """,
-                "source_columns": ["id", "dateAdded", "lastModified"],
-                "page_content_mapper": lambda row: row["title"] + row["url"],
+                "query": sql_loader_firefox_sql_query(),
+                "source_columns": ["source"],
+                "page_content_mapper": lambda row: sql_loader_page_content_mapper(row),
             },
             "copy": {
-                "from": os.path.expanduser("~/.mozilla/firefox/*.default-release/places.sqlite"),
+                "from": sql_loader_firefox_copy_path(),
                 "to": "/tmp/bookworm/firefox.sqlite",
             },
         },
-        # "win32": {},
         "darwin": {
             "bookmark_loader": SQLDatabaseLoader,
             "bookmark_loader_kwargs": {
                 "db": lambda _: SQLDatabase.from_uri("sqlite:////tmp/bookworm/firefox.sqlite"),
-                "query": """
-                    SELECT
-                       CAST(moz_places.id AS TEXT) AS id,
-                       moz_bookmarks.title,
-                       moz_places.url,
-                       CAST(moz_bookmarks.dateAdded AS TEXT) AS dateAdded,
-                       CAST(moz_bookmarks.lastModified AS TEXT) AS lastModified
-                    FROM
-                       moz_bookmarks
-                    LEFT JOIN
-                       moz_places
-                    ON
-                       moz_bookmarks.fk = moz_places.id
-                    WHERE
-                       moz_bookmarks.type = 1
-                    AND
-                       moz_bookmarks.title IS NOT NULL
-                """,
-                "source_columns": ["id", "dateAdded", "lastModified"],
-                "page_content_mapper": lambda row: row["title"] + row["url"],
+                "query": sql_loader_firefox_sql_query(),
+                "source_columns": ["source"],
+                "page_content_mapper": lambda row: sql_loader_page_content_mapper(row),
             },
             "copy": {
-                "from": os.path.expanduser('~/Library/Application Support/Firefox/Profiles/*.default-release/places.sqlite'),
+                "from": sql_loader_firefox_copy_path(),
                 "to": "/tmp/bookworm/firefox.sqlite",
             },
         },
+        # "win32": {},
     },
 }
---
+++ tests/test_utils.py
@@ -0,0 +1,48 @@
+from unittest.mock import Mock, patch
+
+import pytest
+from bookworm_genai.utils import sql_loader_firefox_copy_path, sql_loader_page_content_mapper
+
+
+def test_sql_loader_page_content_mapper():
+    row = {"id": 1, "title": "title", "url": "url", "dateAdded": "dateAdded", "lastModified": "lastModified", "source": "source"}
+
+    result = sql_loader_page_content_mapper(row)
+    assert result == '{"id": 1, "url": "url", "dateAdded": "dateAdded", "lastModified": "lastModified", "source": "source", "name": "title"}'
+
+
+@pytest.mark.parametrize(
+        "platform,mocked_expanduser",
+        [
+            pytest.param(
+                "linux",
+                "/home/user/.mozilla/firefox/*.default-release/places.sqlite",
+                id="linux"
+            ),
+            pytest.param(
+                "darwin",
+                "/Users/user/Library/Application Support/Firefox/Profiles/*.default-release/places.sqlite",
+                id="darwin"
+            ),
+        ]
+)
+@patch("bookworm_genai.utils.os.path.expanduser")
+@patch("bookworm_genai.utils.sys")
+def test_sql_loader_firefox_copy_path_linux(mock_sys: Mock, mock_expanduser: Mock, platform: str, mocked_expanduser: str):
+
+    sql_loader_firefox_copy_path.cache_clear()
+
+    mock_sys.platform = platform
+    mock_expanduser.return_value = mocked_expanduser
+
+    assert sql_loader_firefox_copy_path() == mocked_expanduser
+
+
+@patch("bookworm_genai.utils.sys")
+def test_sql_loader_firefox_copy_path_unknown(mock_sys: Mock):
+    sql_loader_firefox_copy_path.cache_clear()
+
+    mock_sys.platform = "unknown"
+
+    with pytest.raises(NotImplementedError, match="Platform unknown is not supported"):
+        sql_loader_firefox_copy_path()
